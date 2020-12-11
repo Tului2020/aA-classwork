05d7c99c2a26767c614102c059b7f8909a85a2dd
Tului2020 <Tului2020>
2020-12-10 17:35:55 -0800

    uploaded W4D4 work

---
+++ W4D4/poker/lib/deck.rb
@@ -0,0 +1,57 @@
+require_relative "card"
+
+
+class Deck
+
+  attr_reader :cards
+
+  def initialize
+      @cards = add_shuffled
+  end
+
+  def add_shuffled
+    all_cards = Card.valid_suits.product(Card.valid_values).shuffle
+    all_cards.map { |card| Card.new(card[0], card[1])}
+  end
+end
+
+d = Deck.new
+d.add_shuffled
+
+p d.cards.uniq.length
+
+# Five of a Kind – This is the highest possible hand and can occur only in games where at 
+# least one card is wild, such as a joker, the two one-eyed jacks, or the four deuces. 
+# Examples of five of a kind would be four 10s and a wild card or two queens and three wild cards.
+
+# Straight Flush – This is the highest possible hand when only the standard pack is used, and 
+# there are no wild cards. A straight flush consists of five cards of the same suit in sequence, 
+# such as 10, 9, 8, 7, 6 of hearts. The highest-ranking straight flush is the A, K, Q, J, and 10 
+# of one suit, and this combination has a special name: a royal flush or a royal straight flush. 
+# The odds on being dealt this hand are 1 in almost 650,000.
+
+# Four of a Kind – This is the next highest hand, and it ranks just below a straight flush. An 
+# example is four aces or four 3s. It does not matter what the fifth, unmatched card is.
+
+# Full House – This colorful hand is made up of three cards of one rank and two cards of another 
+# rank, such as three 8s and two 4s, or three aces and two 6s.
+
+# Flush – Five cards, all of the same suit, but not all in sequence, is a flush. An example is Q, 
+# 10, 7, 6, and 2 of clubs.
+
+# Straight – Five cards in sequence, but not all of the same suit is a straight. An example is 9♥, 
+# 8♣, 7♠, 6♦, 5♥.
+
+# Three of a Kind – This combination contains three cards of the same rank, and the other two 
+# cards each of a different rank, such as three jacks, a seven, and a four.
+
+# Two Pairs – This hand contains a pair of one rank and another pair of a different rank, plus 
+# any fifth card of a different rank, such as Q, Q, 7, 7, 4.
+
+# One Pair – This frequent combination contains just one pair with the other three cards being 
+# of different rank. An example is 10, 10, K, 4, 3.
+
+# No Pair – This very common hand contains "nothing." None of the five cards pair up, nor are 
+# all five cards of the same suit or consecutive in rank. When more than one player has no pair, 
+# the hands are rated by the highest card each hand contains, so that an ace-high hand beats a 
+# king-high hand, and so on.
