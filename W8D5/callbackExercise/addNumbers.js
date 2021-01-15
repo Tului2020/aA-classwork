727b3a6197199bc32c47a9890dbe71d66a7ad9b5
Tului <tgantulga@ucdavis.edu>
2021-01-15 14:45:30 -0800

    finished absurdBubbleSort

--- W8D5/callbackExercise/addNumbers.js
+++ W8D5/callbackExercise/addNumbers.js
@@ -5,29 +5,24 @@ const reader = readline.createInterface({
     output: process.stdout
 });
 
-//write a func
+
 function addNumbers(sum, numsLeft, completionCallback){
-//     If numsLeft > 0, then:
+
     if(numsLeft > 0){
-//           Prompt the user for a number(use reader).
-//           Pass a callback that:
-//           Uses parseInt to parse the input.
-        reader.question("Enter a number", function(input){
+        reader.question("Enter a number: ", function(input){
             let number = parseInt(input);
             sum += number;
-        }
+            console.log(sum)
+        
             addNumbers(sum, numsLeft-1, completionCallback);
-//            Increment the sum and console.log it.
-//           Recursively calls addNumbers again, passing in:
-//                  the increased sum,
-//                  the decreased numsLeft,
-//                  and the same completionCallback.
-        }
+        });
     }
     else{
         completionCallback(sum)
     }
-//     If numsLeft === 0, call completionCallback(sum) 
-        // so that the total sum can be used.
-
 }
+
+addNumbers(0, 5, function (sum) {
+    console.log('total sum: ' + sum);
+    reader.close()
+})
