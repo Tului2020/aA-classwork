e22b5ccec1f5e9ee1bfac312a541ea23301f54da
Tului <tgantulga@ucdavis.edu>
2021-01-15 16:22:04 -0800

    finished

--- W8D5/callbackExercise/throttleAndDebounce.js
+++ W8D5/callbackExercise/throttleAndDebounce.js
@@ -10,6 +10,90 @@ Function.prototype.myThrottle = function(interval){
     };
 };
 
+
+class SearchBar {
+    constructor() {
+      this.query = "";
+  
+      this.type = this.type.bind(this);
+      this.search = this.search.bind(this);
+    }
+  
+    type(letter) {
+      this.query += letter;
+      this.search();
+    }
+  
+    search() {
+      console.log(`searching for ${this.query}`);
+    }
+}
+
+
+
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+Function.prototype.myDebounce = function(interval) {
+    let timeOut;
+
+    return(...args) => {
+        let fnc = () => {
+            timeOut = null;
+            this(...args);
+        }
+        clearTimeout(timeOut);
+        timeOut = setTimeout(fnc, interval);
+    };
+
+   
+
+};
+
+
+
+const searchBar = new SearchBar();
+
+
+searchBar.search = searchBar.search.myDebounce(5000);
+// searchBar.search()
+
+
+
+
+const queryForHelloWorld = () => {
+  searchBar.type("h");
+  searchBar.type("e");
+  searchBar.type("l");
+  searchBar.type("l");
+  searchBar.type("o");
+  searchBar.type(" ");
+  searchBar.type("w");
+  searchBar.type("o");
+  searchBar.type("r");
+  searchBar.type("l");
+  searchBar.type("d");
+};
+
+
+queryForHelloWorld();
+
+
+
+
 // class Neuron {
 //     fire() {
 //         console.log("Firing!");
